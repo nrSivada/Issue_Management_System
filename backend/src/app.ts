3d8edbc01ebd8080f6940d7a9cbba8d85153c2e9
nrSivada <sivadanrajeev19@gmail.com>
2026-06-07 21:37:01 +0530

    fix neon

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      
       "https://issue-management-system-theta.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import app from "./app";
 
 dotenv.config();
 
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
