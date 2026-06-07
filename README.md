1c6724e88198b5fd772bcd2eef3943035a99a1ff
nrSivada <sivadanrajeev19@gmail.com>
2026-06-07 16:15:53 +0530

    update Readme

--- README.md
+++ README.md
@@ -21,3 +21,34 @@
 - Drizzle ORM
 ### AI:
 -Google Gemini
+
+## Setup instructions
+
+### backend
+
+-install dependencies
+    -cd backend
+    -npm install dependencies
+-environment variables
+    -create a .env file inside backend and refer .env.example file
+-run development
+    -npm run dev
+-build project
+    -npm run build
+-swagger docs
+    -http://localhost:5000/api-docs
+
+### frontend
+
+-install dependencies
+    -cd frontend
+    -npm install dependencies
+-environment variables
+    -create a .env file inside backend and refer .env.example file
+-run development
+    -npm run dev
+
+### Database
+-Create Neon DB
+-copy connection string
+-add to .env
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,16 +14,25 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://issue-management-system-2hyh.onrender.com"
-    ],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://issue-management-system-2hyh.onrender.com"
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
   })
 );
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (_req, res) => {
